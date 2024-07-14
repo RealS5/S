36c4592f1841825692f9d303e7df0b5f98b1ead5
RealS5 <RealSolomonc@gmail.com>
2024-07-13 22:42:52 -0400

    fixed how links work
    it no longer includes .html

--- moreHTML/spooktube/upload.js
+++ moreHTML/spooktube/upload.js
@@ -7,7 +7,7 @@ function upload(file) {
     fileReader.onload = ()=>{
         localStorage.setItem("videoURL", fileReader.result)
         console.log(file);
-        window.location.replace('video.html');
+        window.location.replace('video/index.html');
     }
     fileReader.readAsDataURL(file);
 }
